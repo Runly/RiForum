cce66f03f99375bf4452d10e7f112961fbe37d15
冉力 <ranly314@gmail.com>
2017-01-20 22:26:11 +0800

    注册模块

---
+++ app/ RiForum.py
@@ -0,0 +1,72 @@
+import json
+import time
+from sqlalchemy import func
+from flask import Flask, request
+from app.db.database import *
+
+app = Flask(__name__)
+app.config.from_object('config')
+db_session = None
+
+
+@app.before_request
+def before_request(exception=None):
+    global db_session
+    init_db()
+    db_session = DbSession()
+
+
+@app.teardown_request
+def teardown_request(exception=None):
+    if db_session is not None:
+        db_session.close()
+
+
+@app.route('/sign_in', methods=['POST'])
+def sign_in():
+    error = None
+    if request.method == 'POST':
+        email = None
+        phone = None
+        password = None
+        if 'email' in json.loads(request.get_data()).keys():
+            email = json.loads(request.get_data())['email']
+        if 'phone' in json.loads(request.get_data()).keys():
+            phone = json.loads(request.get_data())['phone']
+        if 'password' in json.loads(request.get_data()).keys():
+            password = json.loads(request.get_data())['password']
+
+        if email is None and phone is None:
+            error = 'email and phone can not be empty at the same time'
+        elif password is None:
+            error = 'password can not be empty'
+        else:
+            user = User(password=password, time=time.time())
+            if email is not None:
+                if db_session.query(User.email).filter(User.email == email).scalar() is not None:
+                    response = Response(data=dict(), code='0', message='email has been already signed in', dateline=time.time())
+                    return json.dumps(response, default=lambda o: o.__dict__)
+                user.email = email
+            else:
+                if db_session.query(User.phone).filter(User.phone == phone).scalar() is not None:
+                    response = Response(data=dict(), code='0', message='phone has been already signed in', dateline=time.time())
+                    return json.dumps(response, default=lambda o: o.__dict__)
+                user.phone = phone
+
+            db_session.add(user)
+            db_session.commit()
+            response = Response(data=user.to_json(), message='sign in successfully', code='1', dateline=time.time())
+            return json.dumps(response, default=lambda o: o.__dict__)
+
+    response = Response(message=error, data=dict(), code='0', dateline=time.time())
+    return json.dumps(response, default=lambda o: o.__dict__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
+
+
---
+++ app/db/database.py
@@ -0,0 +1,71 @@
+# coding=UTF-8
+from sqlalchemy import Column, Integer, String
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from app.utils.permissions import *
+
+Base = declarative_base()
+
+
+def init_db():
+    Base.metadata.create_all(bind=engine)
+
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(15))
+    email = Column(String, unique=True)
+    phone = Column(String(11), unique=True)
+    password = Column(String(16), nullable=False)
+    gender = Column(Integer) #性别
+    birth = Column(String(8))
+    location = Column(String(20))
+    grade = Column(Integer, nullable=False)
+    experience = Column(Integer, nullable=False)
+    time = Column(Integer, nullable=False)
+    permissions = Column(Integer, nullable=False)
+
+    def __init__(self, _id=None, name=None, email=None, phone=None, password='123456', gender=None, birth=None,
+                 user_from=None, grade=1, exp=0, time=0, permissions=USER):
+        self.id = _id
+        self.name = name
+        self.email = email
+        self.phone = phone
+        self.password = password
+        self.gender = gender
+        self.birth = birth
+        self.location = user_from
+        self.grade = grade
+        self.experience = exp
+        self.time = time
+        self.permissions = permissions
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'phone': self.phone,
+            'email': self.email,
+            'password': self.password,
+            'gender': self.gender,
+            'birth': self.birth,
+            'location': self.location,
+            'grade': self.grade,
+            'experience': self.experience,
+            'time': self.time,
+            'permissions': self.permissions
+        }
+
+
+class Response(object):
+    def __init__(self, data=None, code='', message='', dateline=''):
+        self.data = data
+        self.code = code
+        self.message = message
+        self.dateline = dateline
+
+
+engine = create_engine('sqlite:////home/ranly/riforum.db')
+DbSession = sessionmaker(bind=engine)
