ed36e5b4f948ee8e4fbec1138f5c03f9c043ebd8
冉力 <ranly314@gmail.com>
2017-02-05 15:38:51 +0800

    完成发布功能，完善数据库，修改注册接口等

--- app/ RiForum.py
+++ app/ RiForum.py
@@ -30,35 +30,42 @@ def sign_in():
         email = None
         phone = None
         password = None
+        uname = None
         if 'email' in json.loads(request.get_data()).keys():
             email = json.loads(request.get_data())['email']
         if 'phone' in json.loads(request.get_data()).keys():
             phone = json.loads(request.get_data())['phone']
         if 'password' in json.loads(request.get_data()).keys():
             password = json.loads(request.get_data())['password']
+        if 'name' in json.loads(request.get_data()).keys():
+            uname = json.loads(request.get_data())['name']
+
         if email is None and phone is None:
             error = 'email and phone can not be empty at the same time'
-        elif password is None:
-            error = 'password can not be empty'
+        elif password is None and uname is None:
+            error = 'password and name can not be empty'
         else:
-            user = User(password=password, time=time.time())
+            user = User(password=password, name=uname, time=long(time.time()))
             if email is not None:
                 if db_session.query(User.email).filter(User.email == email).scalar() is not None:
-                    response = Response(data=dict(), code='0', message='email has been already signed in', dateline=time.time())
+                    response = Response(code='0', message='email has been already signed in',
+                                        dateline=long(time.time()))
                     return json.dumps(response, default=lambda o: o.__dict__)
                 user.email = email
             else:
                 if db_session.query(User.phone).filter(User.phone == phone).scalar() is not None:
-                    response = Response(data=dict(), code='0', message='phone has been already signed in', dateline=time.time())
+                    response = Response(code='0', message='phone has been already signed in',
+                                        dateline=long(time.time()))
                     return json.dumps(response, default=lambda o: o.__dict__)
                 user.phone = phone
 
             db_session.add(user)
             db_session.commit()
-            response = Response(data=user.to_json(), message='sign in successfully', code='1', dateline=time.time())
+            response = Response(data=user.to_json(), message='sign in successfully', code='1',
+                                dateline=long(time.time()))
             return json.dumps(response, default=lambda o: o.__dict__)
 
-    response = Response(message=error, data=dict(), code='0', dateline=time.time())
+    response = Response(message=error, code='0', dateline=long(time.time()))
     return json.dumps(response, default=lambda o: o.__dict__)
 
 
@@ -83,7 +90,7 @@ def login():
             if email is not None:
                 user = db_session.query(User).filter(User.email == email).one()
                 if user is not None:
-                    token = md5(user.password)
+                    token = md5(user.password + str(long(time.time())))
                     user.token = token
                     db_session.commit()
                     response = Response(data={'uid': user.id, 'token': token}, code='1',
@@ -94,7 +101,7 @@ def login():
             else:
                 user = db_session.query(User).filter(User.phone == phone).one()
                 if user is not None:
-                    token = md5(user.password)
+                    token = md5(user.password + str(long(time.time())))
                     user.token = token
                     db_session.commit()
                     response = Response(data={'uid': user.id, 'token': token}, code='1',
@@ -103,15 +110,68 @@ def login():
                 else:
                     error = 'user is not exists'
 
-    response = Response(message=error, data=dict(), code='0', dateline=long(time.time()))
+    response = Response(message=error, code='0', dateline=long(time.time()))
     return json.dumps(response, default=lambda o: o.__dict__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/release', methods=['POST'])
+def release():
+    error = None
+    if request.method == 'POST':
+        token = None
+        uid = None
+        title = None
+        content = None
+        plate = None
+        sort = None
+        if 'token' in json.loads(request.get_data()).keys():
+            token = json.loads(request.get_data())['token']
+        else:
+            error = 'token is necessary'
+        if 'uid' in json.loads(request.get_data()).keys():
+            uid = json.loads(request.get_data())['uid']
+        else:
+            error = 'uid is necessary'
+        if 'title' in json.loads(request.get_data()).keys():
+            title = json.loads(request.get_data())['title']
+        else:
+            error = 'title is necessary'
+        if 'content' in json.loads(request.get_data()).keys():
+            content = json.loads(request.get_data())['content']
+        else:
+            error = 'content is necessary'
+        if 'plate' in json.loads(request.get_data()).keys():
+            plate = json.loads(request.get_data())['plate']
+        else:
+            error = 'plate is necessary'
+        if 'sort' in json.loads(request.get_data()).keys():
+            sort = json.loads(request.get_data())['plate']
+        else:
+            error = 'sort is necessary'
+
+        if title is None or content is None or uid is None or plate is None or token is None:
+            response = Response(message=error, code='0', dateline=long(time.time()))
+            return json.dumps(response, default=lambda o: o.__dict__)
+        else:
+            user = db_session.query(User).filter(User.id == uid).one()
+            if token != user.token:
+                response = Response(message='not login', code='0', dateline=long(time.time()))
+                return json.dumps(response, default=lambda o: o.__dict__)
+            else:
+                entry = Entries(title=title, content=content,  time=long(time.time()),
+                                uid=uid, uname=user.name, plate=plate, sort=sort)
+                db_session.add(entry)
+                db_session.commit()
+                response = Response(data=entry.to_json(), message='release successfully',
+                                    code='1', dateline=long(time.time()))
+                return json.dumps(response, default=lambda o: o.__dict__)
+
+
+@app.route('/recommend', methods=['GET', 'POST'])
+def recommend():
+    pass
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
-
 
--- db/database.py
+++ db/database.py
@@ -1,9 +1,7 @@
 # coding=UTF-8
-from sqlalchemy import Column, Integer, String
-from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from flask_login import UserMixin
 from utils.permissions import *
 
 Base = declarative_base()
@@ -13,25 +11,25 @@ def init_db():
     Base.metadata.create_all(bind=engine)
 
 
-class User(Base, UserMixin):
+# 用户
+class User(Base):
     __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
     name = Column(String(15))
-    email = Column(String, unique=True)
-    phone = Column(String(11), unique=True)
+    email = Column(String)
+    phone = Column(String(11))
     password = Column(String(16), nullable=False)
     gender = Column(Integer)  # 性别
     birth = Column(String(8))
     location = Column(String(20))
-    grade = Column(Integer, nullable=False)
-    experience = Column(Integer, nullable=False)
-    time = Column(Integer, nullable=False)
-    permissions = Column(Integer, nullable=False)
+    grade = Column(Integer, nullable=False)  # 用户等级
+    experience = Column(Integer, nullable=False)  # 用户经验
+    time = Column(Integer, nullable=False)  # 注册时间
+    permissions = Column(Integer, nullable=False)  # 权限等级
     token = Column(String(32))
 
-    def __init__(self, _id=None, name=None, email=None, phone=None, password='123456', gender=None, birth=None,
-                 user_from=None, grade=1, exp=0, time=0, permissions=USER):
-        self.id = _id
+    def __init__(self, name='', email='', phone='', password='', gender=0,
+                 birth='', user_from='', grade=1, exp=0, time=0, permissions=USER):
         self.name = name
         self.email = email
         self.phone = phone
@@ -60,24 +58,171 @@ class User(Base, UserMixin):
             'permissions': self.permissions
         }
 
-    def is_authenticated(self):
-        return True
 
-    def is_active(self):
-        return True
+# 板块
+class Plate(Base):
+    __tablename__ = 'plate'
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False, unique=True)
+    message = Column(String)
 
-    def is_anonymous(self):
-        return False
+    def __init__(self, name='', message=''):
+        self.name = name
+        self.message = message
 
-    def get_id(self):
-        return unicode(self.id)
+    def to_json(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'message': self.message
+        }
 
-    def __repr__(self):
-        return '<User %r>' % self.__class__.__name__
+
+# 发布的条目
+class Entries(Base):
+    __tablename__ = 'entries'
+    id = Column(Integer, primary_key=True)
+    title = Column(String, nullable=False)
+    content = Column(String, nullable=False)
+    image = Column(String)
+    file_ = Column(String)
+    time = Column(Integer, nullable=False)  # 发布时间
+    uid = Column(Integer, nullable=False)  # 用户id
+    uname = Column(String, nullable=False)  # 用户名字
+    plate = Column(Integer, nullable=False)  # 板块名称
+    sort = Column(Integer, nullable=False)  # 板块类别（新帖， 精华帖， 普通帖等）
+    read_num = Column(Integer)  # 阅读次数
+    like_num = Column(Integer)  # 点赞次数
+    comment_num = Column(Integer)  # 评论次数
+
+    def __init__(self, title='', content='', image='', file_='', time='', uid='',
+                 uname='', plate='', sort='', read_num=0, like_num=0, comment_num=0):
+        self.title = title
+        self.content = content
+        self.image = image
+        self.file_ = file_
+        self.time = time
+        self.uid = uid
+        self.uname = uname
+        self.plate = plate
+        self.sort = sort
+        self.read_num = read_num
+        self.like_num = like_num
+        self.comment_num = comment_num
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'content': self.content,
+            'image': self.image,
+            'file': self.file_,
+            'time': self.time,
+            'uid': self.uid,
+            'uname': self.uname,
+            'plate': self.plate,
+            'sort': self.sort,
+            'read_num': self.read_num,
+            'like_num': self.like_num,
+            'comment_num': self.comment_num
+        }
+
+
+# 评论
+class Comment(Base):
+    __tableName__ = 'comment'
+    id = Column(Integer, primary_key=True)
+    content = Column(String, nullable=False)  # 评论内容
+    plate_id = Column(Integer, nullable=False)  # 板块id
+    entry_id = Column(Integer, nullable=False)  # 被评论的id
+    uid = Column(Integer, nullable=False)  # 评论者id
+    uname = Column(String, nullable=False)  # 评论者name
+    to_uid = Column(Integer, nullable=False)  # 被评论者id
+    to_uname = Column(Integer, nullable=False)  # 被评论者name
+    time = Column(Integer, nullable=False)  # 评论时间
+
+    def __init__(self, content='', plate_id=0, entry_id=0, uid=0, uname='', to_uid=0, to_uname='', time=0):
+        self.content = content
+        self.plate_id = plate_id
+        self.entry_id = entry_id
+        self.uid = uid
+        self.uname = uname
+        self.to_uid = to_uid
+        self.to_uname = to_uname
+        self.time = time
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'content': self.content,
+            'plate_id': self.plate_id,
+            'entry_id': self.entry_id,
+            'uid': self.uid,
+            'uname': self.uname,
+            'to_uid': self.to_uid,
+            'to_uname': self.to_uname,
+            'time': self.time
+        }
+
+
+# 点赞
+class Like(Base):
+    __tablename__ = 'like'
+    id = Column(Integer, primary_key=True)
+    plate_id = Column(Integer, nullable=False)
+    entry_id = Column(Integer, nullable=False)
+    uid = Column(Integer, nullable=False)   # 点赞者id
+    uname = Column(String, nullable=False)  # 点赞者name
+    to_uid = Column(Integer, nullable=False)  # 被点赞者id
+    to_uname = Column(String, nullable=False)  # 被点赞者name
+
+    def __init__(self, plate_id=0, entry_id=0, uid=0, uname='', to_uid=0, to_uname=''):
+        self.plate_id = plate_id
+        self.entry_id = entry_id
+        self.uid = uid
+        self.uname = uname
+        self.to_uid = to_uid
+        self.to_uname = to_uname
+
+    def to_json(self):
+        return {
+            'plate_id': self.plate_id,
+            'entry_id': self.entry_id,
+            'uid': self.uid,
+            'uname': self.uname,
+            'to_uid': self.to_uid,
+            'to_uname': self.uname
+        }
+
+
+# 版主
+class PlateMaster(Base):
+    id = Column(Integer, primary_key=True)
+    uid = Column(Integer, nullable=False)
+    uname = Column(String, nullable=False)
+    plate_id = Column(Integer, nullable=False)
+    flag = Column(Integer, nullable=False)  # 状态(正常或停职)
+    time = Column(Integer, nullable=False)  # 任职时间
+
+    def __init__(self, uid=0, uname='', plate_id=0, flag=MASTER_NORMAL, time=0):
+        self.uid = uid
+        self.uname = uname
+        self.plate_id = plate_id
+        self.flag = flag
+        self.time = time
+
+    def to_json(self):
+        return {
+            'uid': self.uid,
+            'uname': self.uname,
+            'plate_id': self.plate_id,
+            'flag': self.flag,
+            'time': self.time
+        }
 
 
 class Response(object):
-    def __init__(self, data=None, code='', message='', dateline=''):
+    def __init__(self, data=dict(), code='', message='', dateline=''):
         self.data = data
         self.code = code
         self.message = message
